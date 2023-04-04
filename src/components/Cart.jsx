6a9566347bd9efe4e51c7f4cdb4130ba0a05b81a
Diane <dianeyasminec60@gmail.com>
2023-04-04 12:20:24 -0400

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -38,10 +38,11 @@ export default function Cart() {
   <td> ${item.price}</td>
   <td> Quantity: {item.quantity}</td>
   <td>
-    <Button onClick={() => updateItemQuantity(item.id, item.quantity - 1)} variant="outline-dark ms-1">-</Button>
-    <Button onClick={() => updateItemQuantity(item.id, item.quantity + 1)} variant="outline-dark ms-1">+</Button>
-    <Button onClick={() => removeItem(item.id)} variant="outline-danger ms-5">Remove Item</Button>
-  </td>
+  <Button onClick={() => updateItemQuantity(item.id, item.quantity - 1)} variant="outline-dark ms-1">-</Button>
+  <Button onClick={() => updateItemQuantity(item.id, item.quantity + 1)} variant="outline-dark ms-1">+</Button>
+  <Button onClick={() => removeItem(item.id)} variant="outline-danger ms-1 ms-md-5 mt-2 mt-md-0">Remove Item</Button>
+</td>
+
 </tr>
 
                 )
