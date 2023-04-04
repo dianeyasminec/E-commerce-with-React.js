2a541aac9ab2323c8af0debc53f6248f73c326f5
Diane <dianeyasminec60@gmail.com>
2023-04-04 12:28:15 -0400

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,6 @@
 import {Navbar,Nav,Container,Form,Button} from 'react-bootstrap'
 import {Link, Outlet} from 'react-router-dom'
 
-
-
 export default function Layout() {
   return (
     <>
@@ -10,41 +8,36 @@ export default function Layout() {
         <Container fluid>
           <Navbar.Brand > Anitta's Shop</Navbar.Brand>
          
-          <Nav className="me-auto my-2 my-lg-0 "
-            style={{ maxHeight: '100px' }}
-            navbarScroll>
-            <Nav.Link as={Link} to ='/'>Home</Nav.Link>
-            <Nav.Link  as={Link} to ='store'>Store</Nav.Link>
-            <Nav.Link as={Link} to ='teams'>Teams</Nav.Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="store">Store</Nav.Link>
+              <Nav.Link as={Link} to="teams">Teams</Nav.Link>
             </Nav>
-            <Navbar.Collapse className="justify-content-end mx-5">
-                <Nav>
-          <Navbar.Text>
-            <p className="text-light">
-          Everything has beauty, but not everyone sees it
-          </p>
-          </Navbar.Text>
-          </Nav>
-          <Nav>
-          <Form className="d-flex mx-5  ">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2 "
-              aria-label="Search"
-            />
-            <Button variant="outline-secondary ">Search</Button>
-          </Form>
-          </Nav>
-        </Navbar.Collapse>
-         
+            <Navbar.Text>
+              <p className="text-light">
+                Everything has beauty, but not everyone sees it
+              </p>
+            </Navbar.Text>
+            <Form className="d-flex mx-5 my-2 my-lg-0">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-secondary">Search</Button>
+            </Form>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
       <Outlet/>
-      
     </>
-  
+  );
+}
 
-
-
-  )};
